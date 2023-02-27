fd9a590aff963bf61bbb2f7aeee73a5e92d96342
Oleg Mukhin <oleg.mukhin18@mail.ru>
2023-02-28 01:04:54 +0300

    newStructurs

---
+++ HomeWorks JavaScript/HomeWork.array/HomeWork №4.js
@@ -0,0 +1,6 @@
+"use strict mode"
+const str = "Сегодня была распродажа товаров по $10, $20 и $30"
+for (let index = 0; index < array.length; index++) {
+    str.slice(str.indexOf("$"), str.indexOf(" "))
+    
+}
