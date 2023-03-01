a4512fb9678b9a5b73dfb4f7dc03bb7823641c7e
Oleg Mukhin <oleg.mukhin18@mail.ru>
2023-03-02 01:18:38 +0300

    HomeWork.function

--- HomeWorks JavaScript/HomeWork.array/HomeWork №1.js
+++ HomeWorks JavaScript/HomeWork.array/HomeWork №1.js
@@ -2,8 +2,6 @@
 const array = [2, 3, 6, 77, 24, 63, 81, 62, 111, 36]
 let numberMax = 0
 for (let index = 0; index < array.length; index++) {
-    if (numberMax < array[index]) {
-        numberMax = array[index]
-    }
+    if (numberMax < array[index]) numberMax = array[index]
 }
 console.log(numberMax);
