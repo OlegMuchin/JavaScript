1272d58084206fa17530841f10f3c41f72529a45
Oleg Mukhin <oleg.mukhin18@mail.ru>
2023-03-16 01:30:31 +0300

    objects

---
+++ HomeWorks JavaScript/object/Work3.js
@@ -0,0 +1,26 @@
+"use strict mode"
+// Задание 3
+let user = [
+  {
+    name: "John",
+    age: 30,
+  },
+  {
+    name: "Bob",
+    age: 21,
+  },
+  {
+    name: "Anna",
+    age: 19,
+  },
+];
+for (let index = 0; index < user.length; index++) {
+  if (user[index].name == "Bob") {
+    var obj = user[index];
+  }
+  if (user[index].name == "Anna") {
+    user.splice(index, 1);
+  }
+}
+console.log(obj);
+console.log(user);
---
+++ HomeWorks JavaScript/object/Work6.js
@@ -0,0 +1,14 @@
+"use strict mode"
+// Задание 6
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+let sum = 0
+for (let prop in salaries) {
+   sum = sum + salaries[prop]
+}
+console.log(sum);
+// // Другой способ
+// console.log((Object.values(salaries)).reduce((a,b,c)=>a+b+c)-(Object.values(salaries)).length);
